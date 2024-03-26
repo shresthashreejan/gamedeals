3d9374e73f403e3aa6bd957bdd0f4e968428455f
shreejan shrestha <shr.shreejan@gmail.com>
2024-03-26 20:43:43 +0545

    React migration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Home from "./lib/pages/Home";
+
+function App() {
+    return (
+        <>
+            <Home />
+        </>
+    );
+}
+
+export default App;
---
+++ src/lib/components/Tabs.tsx
@@ -0,0 +1,17 @@
+export default function Tabs() {
+    return (
+        <>
+            <div role="tablist" className="tabs tabs-boxed">
+                <a role="tab" className="tab tab-active">
+                    Free Game Deals
+                </a>
+                <a role="tab" className="tab">
+                    Free To Play Games
+                </a>
+                <a role="tab" className="tab">
+                    Game Recommendation
+                </a>
+            </div>
+        </>
+    );
+}
